fafd4fe1e479523f1b7dd46d8c9dd1dfc254ff06
k12re <83732570+k12re@users.noreply.github.com>
2022-05-22 21:32:34 +0200

    took assignment and made changes to fault in code in Question 3

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Question 1
+
+for (var i = 0; i <= 7; i++) {
+  console.log(i);
+}
+
+//Question 2
+
+for (var i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+//Question 3
+
+for (var i = 10; i <= 20; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
